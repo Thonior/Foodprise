5eb6a0050b0e1114e8bddc8c1eb7eabfc2ca6d9a
Xavier Antolin <xavier.antolin@fhios.es>
2013-04-19 16:32:29 +0200

    limpieza

--- public/sql/media.sql
+++
@@ -1,9 +0,0 @@
-CREATE  TABLE `foodprise`.`media` (
-  `id` INT NOT NULL ,
-  `route` VARCHAR(45) NOT NULL ,
-  `large` VARCHAR(45) NULL ,
-  `medium` VARCHAR(45) NULL ,
-  `thumb` VARCHAR(45) NULL ,
-  `type` VARCHAR(45) NOT NULL ,
-  PRIMARY KEY (`id`) );
-
